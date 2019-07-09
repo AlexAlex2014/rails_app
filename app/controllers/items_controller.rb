d570f85afea43ba2675d812d3f5c832f02acc579
Alex <azuev061981@gmail.com>
2019-07-09 18:18:28 +0300

    next commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,39 @@
 class ItemsController < ApplicationController
   def index
-    # @items = Item.all
-    # render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
+
   end
 
-  # def show
-  #   @items = Item.find(params[:id])
-  # end
+  # /items/1 GET
+  def show
+  @item = Item.where(id: params[:id]).first
+  render "items/show"
+  end
 
+  #/items/new GET
+  def new
 
+  end
+
+  #/items/1/edit GET
+  def edit
+
+  end
+
+  # /items POST
   def create
     # @item = Item.create(params[:item])
     # render text: "#{@item.id}: #{@item.name} (#{!@tem.new_record?})"
+    render text: "item created"
+  end
+
+  # /items/1 PUT/
+  def update
+
+  end
+
+  # /items/1 DELETE
+  def destroy
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
-Rails.application.routes.draw do
-  get 'items/index'
+# Rails.application.routes.draw do
+#   get 'items/index'
+#   resources :items
+# end
+
+MyStore::Application.routes.draw do
   resources :items
+
+  # match :controller(/:action(/:id))(.:format)
 end
